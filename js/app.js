d219dee5adecaf67abbaee10edec8a484007c191
Delbert Zou <Delbert Zou>
2017-04-05 21:55:06 +0800

    2017/4/5

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
 'use strict';
 
-window.addEventListener('scroll',function (event) {
-    if (window.scrollY < document.body.clientHeight) {
-        document.getElementById('parallaxBackground').style.transform =
-            'translateY(-' + window.scrollY / document.body.clientHeight * 10 + '%)';
-    }
-    if (window.scrollY > document.body.clientHeight) {
+// For a smooth animation.
 
+var animationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    function (callback) {
+        return setTimeout(callback, 50 / 3);
+    };
+
+
+var cancelAnimationFrame =
+    window.cancelAnimationFrame ||
+    window.mozCancelAnimationFrame ||
+    window.webkitCancelAnimationFrame ||
+    clearTimeout;
+
+var requestId;
+
+// DOM Elements.
+
+var parallaxBackground = document.getElementById('parallaxBackground');
+var education = document.getElementById('education');
+var navigation = document.getElementById('navigation');
+
+
+// Setup all the animations.
+
+(function scrollAnimation() {
+    function scroll() {
+        if (window.scrollY < document.body.clientHeight) {
+            parallaxBackground.style.transform =
+                'translateY(-' + window.scrollY / document.body.clientHeight * 10 + '%)';
+            if (navigation.classList.contains('fixed')) {
+                navigation.classList.remove('fixed');
+            }
+        } else if (window.scrollY >= education.scrollTop) {
+            navigation.classList.add('fixed');
+        }
+        requestId = animationFrame(scroll);
     }
-});
+    requestId = animationFrame(scroll);
+})()
